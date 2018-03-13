3652d4e99cef4e8ef2eb97200ae893e1c6100df8
André Girol <andregirol@gmail.com>
2018-03-13 14:25:32 -0300

    Form + mvc de update

---
+++ resources/views/disciplinas/edit.blade.php
@@ -0,0 +1,11 @@
+<form method="POST" action="/disciplinas/{{ $disciplina->id }}">
+{{ csrf_field() }}
+{{ method_field('patch') }}
+
+Título: <input name="titulo" value="{{ $disciplina->titulo }}"><br><br>
+Ementa: <textarea name="ementa"> 
+{{ $disciplina->ementa }}
+</textarea>
+<br><br>
+<button type="submit"> Salvar </button>
+</form>
